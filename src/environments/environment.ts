2c1951a2f220e71385989d50d730241783c52a11
Cesar Romero <cra1982@gmail.com>
2020-08-02 11:52:15 -0500

    coneccion con el firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDMH5SuzU2yZZHfDVyQAKD6KJZf0KbRdHc",
-    authDomain: "telenoticias-1150.firebaseapp.com",
-    databaseURL: "https://telenoticias-1150.firebaseio.com",
-    projectId: "telenoticias-1150",
-    storageBucket: "telenoticias-1150.appspot.com",
-    messagingSenderId: "966030009784",
-    appId: "1:966030009784:web:0b218fe2f644cd0e825b4d"
+    apiKey: "AIzaSyBQSEIiRZQWcgPCV39Ngww-_DKdYcnYXwg",
+    authDomain: "lolastore-b2e58.firebaseapp.com",
+    databaseURL: "https://lolastore-b2e58.firebaseio.com",
+    projectId: "lolastore-b2e58",
+    storageBucket: "lolastore-b2e58.appspot.com",
+    messagingSenderId: "357212241401",
+    appId: "1:357212241401:web:e8e6537269a7c24ac95128",
+    measurementId: "G-BQXKMF2VXQ"
   }
 };
 
