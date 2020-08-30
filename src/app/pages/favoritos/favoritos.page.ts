2d766ae69ff1184dfcb587a675bb8491629beaa2
Cesar Romero <cra1982@gmail.com>
2020-08-30 13:25:14 -0500

    Update favoritos.page.ts

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -5,36 +5,37 @@ import { ProductoInterface } from '@interfaces/producto.interface';
 import { FirebaseService } from '@services/firebase.service';
 
 @Component({
-  selector: 'app-favoritos',
-  templateUrl: './favoritos.page.html',
-  styleUrls: ['./favoritos.page.scss'],
+	selector: 'app-favoritos',
+	templateUrl: './favoritos.page.html',
+	styleUrls: ['./favoritos.page.scss'],
 })
 export class FavoritosPage implements OnInit {
-  dataShow: any[] = [];
+	dataShow: any[] = [];
 	data: any[] = [];
 	productos: ProductoInterface[] = [];
 	categorias: any[] = [];
 	query: string = "";
-  user: any;
+	user: any;
 	constructor(private firebaseSvc: FirebaseService) { }
 
 	ngOnInit() {
-  }
+	}
 
-  async ionViewWillEnter(){
-    
-    var usuario: any = JSON.parse(localStorage.getItem("APP_USER"));
-    this.firebaseSvc.obtenerId("clientes", usuario.id).subscribe((user) => {
-      this.user = {};
-      this.user =  user[0];
-      this.init();
-    });
-  }
+	async ionViewWillEnter() {
+
+		var usuario: any = JSON.parse(localStorage.getItem("APP_USER"));
+		this.firebaseSvc.obtenerId("clientes", usuario.id).subscribe((user) => {
+			this.user = {};
+			this.user = user[0];
+			this.init();
+		});
+	}
 
 	async init() {
-    this.dataShow = [];
-    this.productos = [];
-    this.productos = this.user.favoritos;
+		this.data = [];
+		this.dataShow = [];
+		this.productos = [];
+		this.productos = this.user.favoritos;
 		this.categorias = await this.firebaseSvc.obtenerPromise('categorias');
 		this.categorias.forEach(element => {
 			const data = { nombre: element.nombre, subcategorias: element.subcategorias };
@@ -42,13 +43,14 @@ export class FavoritosPage implements OnInit {
 				sub['productos'] = this.productos.filter(producto => producto.idunicoSubcategoria == sub.idunico);
 			});
 			this.data.push(data);
-    });
-    this.buscar();
+		});
+		this.buscar();
 	}
 
 	definirSub(categoria) {
 		let dataReturn: Array<any> = [];
-		let subcategoria = {nombre : '', productos: []};
+		this.data = [];
+		let subcategoria = { nombre: '', productos: [] };
 		categoria.subcategorias.forEach(sub => {
 			subcategoria = {
 				nombre: sub.nombre,
@@ -62,24 +64,25 @@ export class FavoritosPage implements OnInit {
 	}
 
 	buscar() {
-    this.dataShow = [];
-    let data: Array<any> = [];
-    this.data.forEach(cat => {
-      let subcategoria = this.definirSub(cat);
-      this.definirSub(cat);
-      data.push({
-        nombre: cat.nombre,
-        subcategorias: subcategoria
-      });
-    });
-    data.forEach((cat, index) => {
-      cat.subcategorias.forEach((sub, index) => {
-        if (sub.productos.length > 0) {
-          cat['tiene'] = true;
-        }
-      });
-    });
-    this.dataShow = data;
+		this.dataShow = [];
+		let data: Array<any> = [];
+		this.data.forEach(cat => {
+			let subcategoria = this.definirSub(cat);
+			this.definirSub(cat);
+			data.push({
+				nombre: cat.nombre,
+				subcategorias: subcategoria
+			});
+		});
+		data.forEach((cat, index) => {
+			cat.subcategorias.forEach((sub, index) => {
+				if (sub.productos.length > 0) {
+					cat['tiene'] = true;
+				}
+			});
+		});
+		console.log(data);
+		this.dataShow = data;
 	}
 
 }
